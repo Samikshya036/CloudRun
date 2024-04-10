39b5f38d212ac43d8df72ba1541aaee63ab0032d
Samikshya036 <113791186+Samikshya036@users.noreply.github.com>
2024-04-10 13:42:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import psycopg2
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Yep!"
+
+
+@app.route("/hello")
+def hello():
+    return "hello"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
